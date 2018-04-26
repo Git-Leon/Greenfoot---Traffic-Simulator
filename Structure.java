89f1a1f43825e13ea9af084ac7ce91f5f6ede1a4
Leon Hunter <xleonhunter@gmail.com>
2018-04-26 19:13:26 -0400

    first commit

---
+++ Structure.java
@@ -0,0 +1,10 @@
+import greenfoot.*;
+public class Structure extends SubActor {
+    public Structure() {
+    }
+
+    public Structure(GreenfootImage image) {
+        setImage(image);
+        rescale();
+    }
+}
